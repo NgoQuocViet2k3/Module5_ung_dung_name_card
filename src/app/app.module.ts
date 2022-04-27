058b26218e01000d8103c7b0361ee35ac22f2697
ruukichi <ngovietlc93@gmail.com>
2022-04-27 10:35:01 +0700

    ung dung name card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NameCarComponent } from './name-car/name-car.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NameCarComponent
   ],
   imports: [
     BrowserModule,
